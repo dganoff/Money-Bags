f2b1b0a53ecdfcf0e508b9422fb159f3b17db773
Demetri Ganoff <dganoff@gmail.com>
2013-08-09 19:43:57 -0400

    Started adding local storage

--- app/js/expenses.js
+++ app/js/expenses.js
@@ -1,11 +1,17 @@
 function ExpensesCtrl($scope) {
+	$scope.saveExpenses = function() {
+		// localStorage["budget.income"] = $scope.income;
+		localStorage["budget.income"] = undefined;
+		console.log('Local storage: budget.income = ' + localStorage["budget.income"]);
+	};
+
 	$scope.expenses = [
 		{name: "Food", amount: 1000},
 		{name: "Internet", amount: 150},
 		{name: "Mortgage", amount: 1500}
 	];
 
-	$scope.income = undefined;
+	$scope.income = localStorage["budget.income"];
 
 	$scope.addExpense = function() {
 		$scope.expenses.push({
@@ -59,14 +65,17 @@ function ExpensesCtrl($scope) {
             'December',
 		],
 		getMonthExpenses: function() {
+			// console.log('running function: getMonthExpenses');
 			var monthExpenses = $scope.total()/$scope.year.months.length;
 			return monthExpenses;
 		},
 		getMonthNet: function() {
+			// console.log('running function: getMonthNet');
 			var net = $scope.income - $scope.total();
 			return net;
 		},
 		getMonthNetClass: function() {
+			// console.log('running function: getMonthNetClass');
 			var className;
 			if ($scope.year.getMonthNet() > 0) {
 				className = 'green';
@@ -74,10 +83,12 @@ function ExpensesCtrl($scope) {
 			return className;
 		},
 		getYearlySavings: function() {
-			for (var i = 0; i < myStringArray.length; i++) {
-			    alert(myStringArray[i]);
-			    //Do something
-			}
+			// console.log('running function: getYearlySavings');
+			var total = $scope.year.getMonthNet() * $scope.year.months.length;
+			// for (var i = 0; i < $scope.year.months.length; i++) {
+			// }
+			console.log(total);
+			return total;
 		}
 	};
 }
